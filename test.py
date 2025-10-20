305014d3b727acda57af8a62172f2ae4b0f972e0
investimentigrugno <investimentigrugno@gmail.com>
2025-10-20 17:25:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ def test_multi_ticker_columns():
 
     tickers = [
         'NASDAQ:AAPL', 'ASX:BHP', 'BMFBOVESPA:PETR4', 'TSX:BMO', 'BVC:CCUCO', 'SSE:600519',
-        'MIL:ENEL', 'CPH:DSV', 'EGX:EGTS', 'HEL:NOKIA', 'EPA:ORA', 'FWB:BMW', 'XETR:HEINZ',
-        'HKEX:0700', 'BSE:RELIANCE', 'IDX:TISI', 'ISE:CRH', 'TSE:7203', 'KRX:005930',
+        'MIL:ENEL', 'CSE:DSV', 'EGX:EGTS', 'HEX:NOKIA', 'EURONEXT:ORA', 'XETR:BMW',
+        'HKEX:0700', 'NSE:RELIANCE', 'IDX:TISI', 'ISE:CRH', 'TSE:7203', 'KRX:005930',
         'KSE:000270', 'LTU:GRG', 'LUX:RTL', 'KLS:GLB', 'BMV:GMEXICOB', 'CAS:IAM', 'AMS:PHIA',
         'NZX:ANZ', 'OBX:YAR', 'LSE:BA', 'PSE:AP', 'WSE:CDR', 'LIS:EDP', 'QSI:QNBK', 'MCX:SBER',
         'SGX:DBS', 'BX:MBK', 'BME:ITX', 'OMX:VITR', 'SWX:NESN', 'TPE:2330', 'ADX:ADCB', 'LSE:HSBA',
