d19e7f107cb40e3420f9cfd9998b38c65b93ac4c
investimentigrugno <investimentigrugno@gmail.com>
2025-10-20 16:58:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -25,11 +25,11 @@ def test_multi_ticker_columns():
 
     columns = [
         'name', 'description', 'country', 'sector', 'close',
-        'market_cap_basic', 'total_revenue_yoy_growth_fq', 'gross_profit_qoq_growth_fq',
-        'net_income_qoq_growth_fq', 'earnings_per_share_diluted_qoq_growth_fq',
+        'market_cap_basic', 'total_revenue_yoy_growth_fq', 'gross_profit_yoy_growth_fq',
+        'net_income_yoy_growth_fq', 'earnings_per_share_diluted_yoy_growth_fq',
         'price_earnings_ttm', 'price_free_cash_flow_ttm', 'total_assets',
         'total_debt', 'shrhldr_s_equity_fq', 'operating_margin',
-        'net_margin_ttm', 'free_cash_flow_qoq_growth_fq'
+        'net_margin_ttm', 'free_cash_flow_yoy_growth_fq'
     ]
 
     results = {}
